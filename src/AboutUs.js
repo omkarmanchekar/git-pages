2ca3a5132489a17b92beab2c57d9e77d3961bd65
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2021-06-14 00:17:25 +0530

    v1

---
+++ src/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+    return (
+        <div style={{backgroundColor:'blue'}}>
+            <h1>ABOUT US </h1>
+        </div>
+    )
+}
+
+export default AboutUs
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div style={{backgroundColor:'yellow'}}>
+            <h1 >HOMEEEE</h1>  
+        </div>
+    )
+}
+
+export default Home
